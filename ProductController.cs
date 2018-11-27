17eeb7206c0c5a232173e8f3cebabecf1163a560
Sagar More <45047025+sagar0818more@users.noreply.github.com>
2018-11-27 14:12:54 +0530

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConsumeWebApi.Repository;
+using System.Net.Http;
+
+namespace ConsumeWebApi.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Models.Product product)
+        {
+            ServiceRepository serviceObj = new ServiceRepository();
+            HttpResponseMessage response = serviceObj.PostResponse("api/showroom/Insert", product);
+            response.EnsureSuccessStatusCode();
+            return View();
+        }
+    }
+}
---
+++ ServiceRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace ConsumeWebApi.Repository
+{
+    public class ServiceRepository
+    {
+        public HttpClient Client { get; set; }
+        public ServiceRepository()
+        {
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri(ConfigurationManager.AppSettings["ServiceUrl"].ToString());
+        }
+        public HttpResponseMessage PostResponse(string url, object model)
+        {
+            return Client.PostAsJsonAsync(url, model).Result;
+        }
+       
+    }
+}
